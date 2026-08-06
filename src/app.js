3c606c9d9e9f03b0c060515bf9c6412f98aba1c4
gauravadhikari <adhikarigaurav05@gmail.com>
2026-08-06 13:57:51 +0530

    Update CORS for production

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(cors({
   origin : ["http://localhost:5173",
-  "https://expense-tracker-frontend-nine-theta.vercel.app/"
+  "https://expense-tracker-frontend-nine-theta.vercel.app"
   ],
   credentials:true
 })
