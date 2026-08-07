68d45ba6db7d2b69a00c185698f1fc8e1b46e6a7
gauravadhikari <adhikarigaurav05@gmail.com>
2026-08-07 15:41:52 +0530

    Updating UX

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { signup,login } = require("../controllers/authController");
 const   signupValidator  = require("../validators/authValidator");
 const  validate  = require("../middlewares/validateMiddleware");
-
+const authMiddleware = require("../middlewares/authMiddleware"); 
 router.post("/signup",signupValidator,validate,signup)
 router.post("/login", login)
 router.put("/change-password", authMiddleware, changePassword);
